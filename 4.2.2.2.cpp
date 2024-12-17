fdb4f46a561f2b281fe0a416fe07e8adb01d8047
zeata07 <1904549036@qq.com>
2024-12-17 22:50:15 +0800

    Add files via upload

---
+++ 4.2.2.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int parseHex(const char* const hexString) 
+{
+	int num = 0;
+	int len = strlen(hexString);
+	for (int i = 0; i < len; i++)
+	{
+		if (hexString[i] >= '0' && hexString[i] <= '9')//对于0-9的数值
+		{
+			num = num + ((int(hexString[i])) - 48) * pow(16, len - 1 - i);//转换成int类型的0-9 根据相应位数乘上16的x-1次方
+		}
+		else if (hexString[i] >= 'A' && hexString[i] <= 'F')//对于A-F的数值
+		{
+			num = num + ((int(hexString[i])) - 55) * pow(16, len - 1 - i);//转换为int类型的10-15 根据相应位数乘上16的x-1次方
+		}
+		else//不正确输入
+			cout << "不要乱输" << endl;
+	}
+	return num;
+}
+int main()
+{
+	int t;
+	char s[100];
+	cout << "输入十六进制数" << endl;
+	cin.getline(s, 100);
+	t=parseHex(s);
+	cout << "十进制数为" << t << endl;
+	return 0;
+}
---
+++ 4.2.2.3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+void bubbleSort(int* arr, int a)//调用之前的升序函数
+{
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < a - i; j++)
+		{
+			if (arr[j] > arr[j + 1] && j + 1 < a)//这里注意要加上限，否则j+1是超小数就完了
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int a,*arr;
+	cout << "请输入数组的数目" << endl;
+	cin >> a;
+	arr = new int[a];
+	cout << "请输入数组" << endl;
+	for (int i = 0; i < a; i++)
+	{
+		cin >> arr[i];
+	}
+	bubbleSort(arr, a);
+	for (int i = 0; i < a; i++)
+	{
+		cout << *&arr[i] << " ";//用指针输出
+	}
+	delete[]arr;//释放内存空间
+	return 0;
+}
